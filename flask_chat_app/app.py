6f35f7372c2d0f712e32e30320d6ebc120d59c07
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-07-11 00:19:13 +0300

    remove pymongo temporarily

--- flask_chat_app/app.py
+++ flask_chat_app/app.py
@@ -12,16 +12,16 @@ from flask_login import (
     current_user,
 )
 
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-from bson.objectid import ObjectId
+# from pymongo import MongoClient
+# from pymongo.server_api import ServerApi
+# from bson.objectid import ObjectId
 from gevent.pywsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
 
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app, async_mode="gevent")
 
 
@@ -30,16 +30,44 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # MongoDB configuration
-MONGO_URI = os.getenv("MONGO_URI")
-logging.info(f"Connecting to MongoDB at {MONGO_URI}")
+# MONGO_URI = os.getenv("MONGO_URI")
+# logging.info(f"Connecting to MongoDB at {MONGO_URI}")
 
-client = MongoClient(
-    MONGO_URI,
-    server_api=ServerApi("1"),
-)
-db = client["flaskchatapp"]
-chat_collection = db["chat"]
-user_collection = db["users"]
+# client = MongoClient(
+#     MONGO_URI,
+#     server_api=ServerApi("1"),
+# )
+# db = client["flaskchatapp"]
+# chat_collection = db["chat"]
+# user_collection = db["users"]
+
+
+# In-memory database
+chat_collection = [
+    {
+        "text": "Hello",
+        "userId": "1",
+    },
+]
+user_collection = [
+    {
+        "_id": "1",
+        "username": "youssef",
+        "password": "1",
+    },
+    {
+        "_id": "2",
+        "username": "omar",
+        "password": "1",
+    },
+]
+
+
+def find_user_by(key, value):
+    for user in user_collection:
+        if user[key] == value:
+            return user
+    return None
 
 
 # User model
@@ -59,7 +87,7 @@ class User(UserMixin):
 def load_user(user_id):
     assert isinstance(user_id, str), "user_id must be a string"
 
-    user = user_collection.find_one({"_id": ObjectId(user_id)})
+    user = find_user_by("_id", user_id)
     assert user, "User must exist"
 
     username = user["username"]
@@ -78,19 +106,18 @@ def unauthorized_callback():
 @app.route("/")
 @login_required
 def index():
-    messages = chat_collection.find()
+    messages = chat_collection
     processed_messages = []
     for message in messages:
         user_id = str(message["userId"])
         assert isinstance(user_id, str), "user_id must be a string"
 
-        user = user_collection.find_one({"_id": ObjectId(user_id)})
+        user = find_user_by("_id", user_id)
         assert user, "User must exist"
 
         processed_message = {
             "text": message["text"],
             "user": {"username": user["username"]},
-            "timestamp": message["timestamp"],
         }
         processed_messages.append(processed_message)
     return render_template("index.html", messages=processed_messages)
@@ -101,7 +128,7 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        user = user_collection.find_one({"username": username})
+        user = find_user_by("username", username)
         if user and user["password"] == password:
             user_id = str(user["_id"])
             user_obj = User(user_id, username)
@@ -118,17 +145,18 @@ def register():
         username = request.form["username"]
         password = request.form["password"]
         # Check if the username is already taken
-        if user_collection.find_one({"username": username}):
+        if find_user_by("username", username):
             flash(
                 "Username already exists. Please choose a different username.", "error"
             )
         else:
             # Create a new user document
             new_user = {
+                "_id": str(len(user_collection) + 1),
                 "username": username,
                 "password": password,
             }
-            user_collection.insert_one(new_user)
+            user_collection.append(new_user)
             flash("Registration successful. Please log in.", "success")
             return redirect(url_for("login"))
     return render_template("register.html")
@@ -150,8 +178,8 @@ def handle_message(message):
         "text": message["text"],
         "userId": user_id,
     }
-    chat_collection.insert_one(new_message)
-    del new_message["_id"]
+    chat_collection.append(new_message)
+    # del new_message["_id"]
 
     emit(
         "message",
